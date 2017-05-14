8b273e46a701d7b395f4ce19f119a1251ae06ca6
Lee Richardson <github@leerichardson.com>
2017-05-14 14:46:26 -0400

    Demonstrating Centering in traditional programtic RelativeLayout

--- EasyLayout.Sample/EasyLayout.Sample/Views/TraditionalRelativeLayout.xaml.cs
+++ EasyLayout.Sample/EasyLayout.Sample/Views/TraditionalRelativeLayout.xaml.cs
@@ -25,11 +25,12 @@ namespace EasyLayout.Sample.Views
 
             var mainLabel = relativeLayout.AddLabel("MainLabel", Colors.BluePurple, Colors.DarkGrey);
 	        var relativeLabel = relativeLayout.AddLabel("RelativeLabel", Colors.Red, Colors.DarkGrey);
+	        var centerLabel = relativeLayout.AddLabel("CenterLabel", Colors.Green, Colors.White);
 
             relativeLayout.Children.Add(mainLabel, 
                 Constraint.RelativeToParent(rl => rl.X + 10),
                 Constraint.RelativeToParent(rl => rl.Y + 10),
-                Constraint.Constant(100),
+                Constraint.Constant(150),
                 Constraint.Constant(40)
                 );
 
@@ -39,6 +40,16 @@ namespace EasyLayout.Sample.Views
                 Constraint.RelativeToView(mainLabel, (rl, v) => v.Width),
                 Constraint.RelativeToView(mainLabel, (rl, v) => v.Height)
                 );
+
+            Size GetSize(VisualElement ve, RelativeLayout rl) => ve.Measure(rl.Width, rl.Height).Request;
+
+            relativeLayout.Children.Add(centerLabel, 
+                Constraint.RelativeToView(relativeLabel, (rl, v) => v.X + (v.Width * .5f) - (GetSize(centerLabel, rl).Width / 2)),
+                Constraint.RelativeToView(relativeLabel, (rl, v) => v.Y + v.Height + 10)
+                //,Constraint.RelativeToView(mainLabel, (rl, v) => v.Width)
+                // ^ this breaks RelativeLayout
+                );
+
 
         }
 	}
