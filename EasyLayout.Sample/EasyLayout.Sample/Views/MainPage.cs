7bafceb9edf62dce269142954832d65861cb0f08
Lee Richardson <github@leerichardson.com>
2017-05-14 13:35:08 -0400

    A traditional XAML based relative layout

--- EasyLayout.Sample/EasyLayout.Sample/Views/MainPage.cs
+++ EasyLayout.Sample/EasyLayout.Sample/Views/MainPage.cs
@@ -1,6 +1,8 @@
-﻿﻿using EasyLayout.Forms.Sample;
+﻿﻿using System;
+ using EasyLayout.Forms.Sample;
 using EasyLayout.Forms;
-using Xamarin.Forms;
+ using EasyLayout.Sample.Views;
+ using Xamarin.Forms;
 
 namespace EasyLayout.Sample
 {
@@ -8,6 +10,7 @@ namespace EasyLayout.Sample
     {
         private RelativeLayout _relativeLayout;
         private Button _layoutExampleButton;
+        private Button _traditionalXamlRelativeLayout;
 
         public RootPage()
         {
@@ -25,11 +28,19 @@ namespace EasyLayout.Sample
         protected override void OnAppearing()
         {
             _layoutExampleButton.Clicked += LayoutExampleButton_Clicked;
+            _traditionalXamlRelativeLayout.Clicked += TraditionalXamlRelativeLayoutOnClicked;
         }
 
         protected override void OnDisappearing()
         {
             _layoutExampleButton.Clicked -= LayoutExampleButton_Clicked;
+            _traditionalXamlRelativeLayout.Clicked -= TraditionalXamlRelativeLayoutOnClicked;
+        }
+
+        private void TraditionalXamlRelativeLayoutOnClicked(object sender, EventArgs eventArgs)
+        {
+            var traditionalRelativeLayout = new TraditionalRelativeLayout();
+            Navigation.PushAsync(traditionalRelativeLayout);
         }
 
         private void LayoutExampleButton_Clicked(object sender, System.EventArgs e)
@@ -43,7 +54,11 @@ namespace EasyLayout.Sample
             _relativeLayout.ConstrainLayout(() =>
                 _layoutExampleButton.Bounds.Top == _relativeLayout.Bounds.Top + 20 &&
                 _layoutExampleButton.Bounds.Left == _relativeLayout.Bounds.Left + 10 &&
-                _layoutExampleButton.Bounds.Right == _relativeLayout.Bounds.Right - 10
+                _layoutExampleButton.Bounds.Right == _relativeLayout.Bounds.Right - 10 &&
+                
+                _traditionalXamlRelativeLayout.Bounds.Top == _layoutExampleButton.Bounds.Bottom + 10 &&
+                _traditionalXamlRelativeLayout.Bounds.Left == _layoutExampleButton.Bounds.Left &&
+                _traditionalXamlRelativeLayout.Bounds.Width == _layoutExampleButton.Bounds.Width
             );
         }
 
@@ -51,6 +66,7 @@ namespace EasyLayout.Sample
         {
             _relativeLayout = ViewUtils.AddRelativeLayout();
             _layoutExampleButton = _relativeLayout.AddButton("Layout Example");
+            _traditionalXamlRelativeLayout = _relativeLayout.AddButton("Traditional XAML RelativeLayout");
         }
     }
 
---
+++ EasyLayout.Sample/EasyLayout.Sample/Views/TraditionalRelativeLayout.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EasyLayout.Sample.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TraditionalRelativeLayout : ContentPage
+	{
+		public TraditionalRelativeLayout ()
+		{
+			InitializeComponent ();
+		    Title = "Traditional XAML RelativeLayout";
+		}
+	}
+}
