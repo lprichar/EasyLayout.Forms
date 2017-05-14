4d4d862d7613bb0b981db335aca92c0b658d0a95
Lee Richardson <github@leerichardson.com>
2017-05-14 14:04:19 -0400

    Traditional programatic RelativeLayout

--- EasyLayout.Sample/EasyLayout.Sample/Views/TraditionalRelativeLayout.xaml.cs
+++ EasyLayout.Sample/EasyLayout.Sample/Views/TraditionalRelativeLayout.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EasyLayout.Forms.Sample;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +16,30 @@ namespace EasyLayout.Sample.Views
 		{
 			InitializeComponent ();
 		    Title = "Traditional XAML RelativeLayout";
+		    AddViews();
 		}
+
+	    private void AddViews()
+	    {
+	        var relativeLayout = InnerRelativeLayout;
+
+            var mainLabel = relativeLayout.AddLabel("MainLabel", Colors.BluePurple, Colors.DarkGrey);
+	        var relativeLabel = relativeLayout.AddLabel("RelativeLabel", Colors.Red, Colors.DarkGrey);
+
+            relativeLayout.Children.Add(mainLabel, 
+                Constraint.RelativeToParent(rl => rl.X + 10),
+                Constraint.RelativeToParent(rl => rl.Y + 10),
+                Constraint.Constant(100),
+                Constraint.Constant(40)
+                );
+
+            relativeLayout.Children.Add(relativeLabel, 
+                Constraint.RelativeToView(mainLabel, (rl, v) => v.X + v.Width + 10),
+                Constraint.RelativeToView(mainLabel, (rl, v) => v.Y + v.Height + 10),
+                Constraint.RelativeToView(mainLabel, (rl, v) => v.Width),
+                Constraint.RelativeToView(mainLabel, (rl, v) => v.Height)
+                );
+
+        }
 	}
 }
