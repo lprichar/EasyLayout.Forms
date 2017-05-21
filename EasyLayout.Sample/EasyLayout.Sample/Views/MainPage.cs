605bf85beea2b64e4606fb08ee8c6e6ff345b6e0
Lee Richardson <github@leerichardson.com>
2017-05-21 07:32:55 -0400

    Three new pages, 2 for comparing performance, 1 for a playground

--- EasyLayout.Sample/EasyLayout.Sample/Views/MainPage.cs
+++ EasyLayout.Sample/EasyLayout.Sample/Views/MainPage.cs
@@ -11,6 +11,9 @@ namespace EasyLayout.Sample
         private RelativeLayout _relativeLayout;
         private Button _layoutExampleButton;
         private Button _traditionalXamlRelativeLayout;
+        private Button _traditionalPerformance;
+        private Button _elxfPerformance;
+        private Button _playground;
 
         public RootPage()
         {
@@ -29,12 +32,36 @@ namespace EasyLayout.Sample
         {
             _layoutExampleButton.Clicked += LayoutExampleButton_Clicked;
             _traditionalXamlRelativeLayout.Clicked += TraditionalXamlRelativeLayoutOnClicked;
+            _traditionalPerformance.Clicked += TraditionalPerformance_Clicked;
+            _elxfPerformance.Clicked += ElxfPerformance_Clicked;
+            _playground.Clicked += Playground_Clicked;
         }
 
         protected override void OnDisappearing()
         {
             _layoutExampleButton.Clicked -= LayoutExampleButton_Clicked;
             _traditionalXamlRelativeLayout.Clicked -= TraditionalXamlRelativeLayoutOnClicked;
+            _traditionalPerformance.Clicked -= TraditionalPerformance_Clicked;
+            _elxfPerformance.Clicked -= ElxfPerformance_Clicked;
+            _playground.Clicked -= Playground_Clicked;
+        }
+
+        private void Playground_Clicked(object sender, EventArgs e)
+        {
+            var playgroundPage = new PlaygroundPage();
+            Navigation.PushAsync(playgroundPage);
+        }
+
+        private void TraditionalPerformance_Clicked(object sender, EventArgs e)
+        {
+            var traditionalPerformancePage = new TraditionalPerformancePage();
+            Navigation.PushAsync(traditionalPerformancePage);
+        }
+
+        private void ElxfPerformance_Clicked(object sender, EventArgs e)
+        {
+            var elxfPerformancePage = new ElxfPerformancePage();
+            Navigation.PushAsync(elxfPerformancePage);
         }
 
         private void TraditionalXamlRelativeLayoutOnClicked(object sender, EventArgs eventArgs)
@@ -52,13 +79,25 @@ namespace EasyLayout.Sample
         private void ConstrainLayout()
         {
             _relativeLayout.ConstrainLayout(() =>
-                _layoutExampleButton.Bounds.Top == _relativeLayout.Bounds.Top + 20 &&
-                _layoutExampleButton.Bounds.Left == _relativeLayout.Bounds.Left + 10 &&
-                _layoutExampleButton.Bounds.Right == _relativeLayout.Bounds.Right - 10 &&
-                
-                _traditionalXamlRelativeLayout.Bounds.Top == _layoutExampleButton.Bounds.Bottom + 10 &&
-                _traditionalXamlRelativeLayout.Bounds.Left == _layoutExampleButton.Bounds.Left &&
-                _traditionalXamlRelativeLayout.Bounds.Width == _layoutExampleButton.Bounds.Width
+                 _layoutExampleButton.Bounds.Top == _relativeLayout.Bounds.Top + 20
+                && _layoutExampleButton.Bounds.Left == _relativeLayout.Bounds.Left + 10
+                && _layoutExampleButton.Bounds.Right == _relativeLayout.Bounds.Right - 10
+
+                && _traditionalXamlRelativeLayout.Bounds.Top == _layoutExampleButton.Bounds.Bottom + 10
+                && _traditionalXamlRelativeLayout.Bounds.Left == _layoutExampleButton.Bounds.Left
+                && _traditionalXamlRelativeLayout.Bounds.Width == _layoutExampleButton.Bounds.Width
+
+                && _traditionalPerformance.Bounds.Top == _traditionalXamlRelativeLayout.Bounds.Bottom + 10
+                && _traditionalPerformance.Bounds.Left == _layoutExampleButton.Bounds.Left
+                && _traditionalPerformance.Bounds.Width == _layoutExampleButton.Bounds.Width
+
+                && _elxfPerformance.Bounds.Top == _traditionalPerformance.Bounds.Bottom + 10
+                && _elxfPerformance.Bounds.Left == _layoutExampleButton.Bounds.Left
+                && _elxfPerformance.Bounds.Width == _layoutExampleButton.Bounds.Width
+
+                && _playground.Bounds.Top == _elxfPerformance.Bounds.Bottom + 10
+                && _playground.Bounds.Left == _layoutExampleButton.Bounds.Left
+                && _playground.Bounds.Width == _layoutExampleButton.Bounds.Width
             );
         }
 
@@ -67,6 +106,9 @@ namespace EasyLayout.Sample
             _relativeLayout = ViewUtils.AddRelativeLayout();
             _layoutExampleButton = _relativeLayout.AddButton("Layout Example");
             _traditionalXamlRelativeLayout = _relativeLayout.AddButton("Traditional RelativeLayout vs EasyLayout");
+            _traditionalPerformance = _relativeLayout.AddButton("Traditional Performance");
+            _elxfPerformance = _relativeLayout.AddButton("EasyLayout.Forms Performance");
+            _playground = _relativeLayout.AddButton("Playground");
         }
     }
 
---
+++ EasyLayout.Sample/EasyLayout.Sample/Views/PlaygroundPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace EasyLayout.Sample.Views
+{
+    public class PlaygroundPage : ContentPage
+    {
+    }
+}
