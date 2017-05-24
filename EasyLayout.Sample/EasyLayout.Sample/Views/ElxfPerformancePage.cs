c27314c6c38bfd73bb350e04152b2d5bb36d1d3a
Lee Richardson <github@leerichardson.com>
2017-05-24 00:57:18 -0400

    Added an initial view cell for the list view based on a relative layout, except the performance is terrible :|

--- EasyLayout.Sample/EasyLayout.Sample/Views/ElxfPerformancePage.cs
+++ EasyLayout.Sample/EasyLayout.Sample/Views/ElxfPerformancePage.cs
@@ -1,4 +1,6 @@
 ﻿// ReSharper disable CompareOfFloatsByEqualityOperator
+
+using EasyLayout.Droid.Sample.Models;
 using EasyLayout.Forms;
 using EasyLayout.Forms.Sample;
 using Xamarin.Forms;
@@ -10,6 +12,7 @@ namespace EasyLayout.Sample.Views
         private RelativeLayout _relativeLayout;
         private BoxView _topFrame;
         private Label _perfLabel;
+        private ListView _productsListView;
 
         public ElxfPerformancePage()
         {
@@ -29,6 +32,17 @@ namespace EasyLayout.Sample.Views
             _relativeLayout = ViewUtils.AddRelativeLayout();
             _topFrame = _relativeLayout.AddBoxView(Colors.Background);
             _perfLabel = _relativeLayout.AddLabel("Click The Buttons To View Perf Stats", Color.White);
+            _productsListView = GetProductsListView();
+        }
+
+        private static ListView GetProductsListView()
+        {
+            var listView = new ListView(ListViewCachingStrategy.RetainElement)
+            {
+                ItemsSource = Data.GetProducts(),
+                ItemTemplate = new DataTemplate(typeof(ProductsListCell)),
+            };
+            return listView;
         }
 
         private void ConstrainLayout()
@@ -41,7 +55,52 @@ namespace EasyLayout.Sample.Views
 
                 && _perfLabel.Bounds.Top == _relativeLayout.Bounds.Top + 100
                 && _perfLabel.Bounds.Right == _relativeLayout.Bounds.Right - 10
+
+                && _productsListView.Bounds.Top == _topFrame.Bounds.Bottom
+                && _productsListView.Bounds.Left == _relativeLayout.Bounds.Left
+                && _productsListView.Bounds.Right == _relativeLayout.Bounds.Right
+                && _productsListView.Bounds.Height == _relativeLayout.Bounds.Height - 200
             );
+        }
+    }
+
+    public class ProductsListCell : ViewCell
+    {
+        private RelativeLayout _relativeLayout;
+        private Label _titleLabel;
+
+        public ProductsListCell()
+        {
+            AddViews();
+            ConstrainLayout();
+            View = _relativeLayout;
+        }
+
+        private void AddViews()
+        {
+            _relativeLayout = ViewUtils.AddRelativeLayout();
+            _titleLabel = AddLabel("Title");
+        }
+
+        private static Label AddLabel(string textBinding)
+        {
+            var label = new Label();
+            label.SetBinding(Label.TextProperty, textBinding);
+            return label;
+        }
+
+        private void ConstrainLayout()
+        {
+            _relativeLayout.Children.Add(_titleLabel,
+                () => _relativeLayout.X,
+                () => _relativeLayout.Y);
+
+            //_relativeLayout.ConstrainLayout(() =>
+            //    _titleLabel.Bounds.Left == _relativeLayout.Bounds.Left
+            //    && _titleLabel.Bounds.Top == _relativeLayout.Bounds.Top
+            //    && _titleLabel.Bounds.Width == _relativeLayout.Bounds.Width
+            //    && _titleLabel.Bounds.Height == _relativeLayout.Bounds.Height
+            //    );
         }
     }
 }
