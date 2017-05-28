491fbee3260b545ac1a7b4444bf68bf669d29c4c
Lee Richardson <github@leerichardson.com>
2017-05-27 21:08:59 -0400

    Updated PlaygroundPage to make it a better jumping off point

--- EasyLayout.Sample/EasyLayout.Sample/Views/PlaygroundPage.cs
+++ EasyLayout.Sample/EasyLayout.Sample/Views/PlaygroundPage.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// ReSharper disable CompareOfFloatsByEqualityOperator
+using EasyLayout.Forms;
+using EasyLayout.Forms.Sample;
 using Xamarin.Forms;
 
 namespace EasyLayout.Sample.Views
 {
     public class PlaygroundPage : ContentPage
     {
+        private RelativeLayout _relativeLayout;
+        private Label _helloLabel;
+
+        public PlaygroundPage()
+        {
+            AddViews();
+            SetPageProperties();
+            ConstrainLayout();
+        }
+
+        private void ConstrainLayout()
+        {
+            _relativeLayout.ConstrainLayout(() =>
+                _helloLabel.Bounds.GetCenterX() == _relativeLayout.Bounds.GetCenterX()
+                && _helloLabel.Bounds.Top == _relativeLayout.Bounds.Top + 10
+            );
+        }
+
+        private void AddViews()
+        {
+            _relativeLayout = ViewUtils.AddRelativeLayout();
+            _helloLabel = _relativeLayout.AddLabel("Hello World");
+        }
+
+        private void SetPageProperties()
+        {
+            Content = _relativeLayout;
+        }
     }
 }
