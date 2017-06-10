a0cbccae0df01dc5877ee25ead0f197054baa916
Lee Richardson <github@leerichardson.com>
2017-06-09 23:32:03 -0400

    Added a unit test project and an initial unit test

---
+++ EasyLayout.Forms.Test/EasyLayoutTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.Forms;
+
+namespace EasyLayout.Forms.Test
+{
+    [TestFixture]
+    public class EasyLayoutTest
+    {
+        [Test]
+        public void GivenWrongLeftHandMethodName_WhenConstrainLayout_ThenNiceErrorMessage()
+        {
+            var relativeLayout = new RelativeLayout();
+            var label = new Label();
+            var ex = Assert.Throws<NotSupportedException>(() =>
+                relativeLayout.ConstrainLayout(() =>
+                    label.FontSize == label.Bounds.Left
+                )
+            );
+            Assert.AreEqual("Property FontSize is not recognized.", ex.Message);
+        }
+    }
+}
---
+++ EasyLayout.Forms.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EasyLayout.Forms.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EasyLayout.Forms.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8423868a-845c-42f0-ad6e-554af46ccaf0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
